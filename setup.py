757a4362083dc094d65411d569036d37e5a5f465
Pete <github@kthxbye.us>
2021-04-15 00:15:35 +0800

    FIX: spelling mistakes and version

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='pyuhand',
-      version='0.1',
+      version='0.0.1',
       description='Library to control uhand',
       url='http://github.com/peteh/pyuhand',
-      author='Peter Hofmamm',
+      author='Peter Hofmann',
       author_email='github@kthxbye.us',
       license='LGPL2.1',
       packages=['pyuhand'],
