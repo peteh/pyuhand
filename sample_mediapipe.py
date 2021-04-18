92f31e0a928f70163b78ef64a79a527d6af5c3c5
Pete <github@kthxbye.us>
2021-04-18 18:07:05 +0800

    CHG: refactor media_pipe example

--- sample_mediapipe.py
+++ sample_mediapipe.py
@@ -6,35 +6,27 @@ import cv2
 import mediapipe as mp
 import math
 
-from filterpy.kalman import KalmanFilter
-from filterpy.common import Q_discrete_white_noise , kinematic_kf
+from filterpy.common import kinematic_kf
 
-mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
-
-uhand = pyuhand.UHand("/dev/ttyUSB0")
-
-timedelta = 0.06
-
-class FingerTracker(object):
-    def __init__(self, dt):
-        self._tracks = {}
-        for i in range(4):
-            self._tracks[i] = KalFilter3D(dt)
+class KeyPointFilter(object):
+    def __init__(self, nKeypoints, td):
+        self._filters = []
+        for i in range(nKeypoints):
+            self._filters.append(KalFilter3D(td))
     
     def predict(self):
-        for i in range(len(self._tracks)):
-            self._tracks[i].predict()
+        for kalFilter in self._filters:
+            kalFilter.predict()
     
-    def update(self, point1, point2, point3, point4):
-        self._tracks[0].update(point1)
-        self._tracks[1].update(point2)
-        self._tracks[2].update(point3)
-        self._tracks[3].update(point4)
+    def update(self, keypoints):
+        for i in range(len(self._filters)):
+            self._filters[i].update(keypoints[i])
     
-    def getState(self, index):
-        return self._tracks[index].getStatePos()
-
+    def getFilteredPoints(self):
+        keypoints = []
+        for kalFilter in self._filters:
+            keypoints.append(kalFilter.getStatePos())
+        return keypoints
 
 class KalFilter3D(object):
     def __init__(self, dt, initX = 0, initY=0, initZ=0):
@@ -83,98 +75,104 @@ class VarMeasure(object):
             return 0
         return math.sqrt(np.var(self._store))
 
-lastposition = {}
-lastposition[1] = 0
-lastposition[2] = 0
-lastposition[3] = 0
-lastposition[4] = 0
-lastposition[5] = 0
-lastTime = time.time()
+def calculateHandAngle(base, direction):
+    zAxis = np.array([0., 0., 1.])
+    xAxis2d = np.array([1., 0.])
+    directionVec = direction - base
+    directionVec2d = np.array([directionVec[0], directionVec[2]])
+    handAngleDeg = angle2(xAxis2d, directionVec2d)
+    handAnglePercent = handAngleDeg / 360. * 100.
+    return handAnglePercent
 
-fingers = {}
-fingers[1] = FingerTracker(timedelta)
-fingers[2] = FingerTracker(timedelta)
-fingers[3] = FingerTracker(timedelta)
-fingers[4] = FingerTracker(timedelta)
-fingers[5] = FingerTracker(timedelta)
+class HandAngle(object):
 
-varMeasure = VarMeasure(100)
-
-lastKeyPoints = None
-def angle(v1, v2, acute = True):
-    angle = np.arccos(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
-    if (acute == True):
-        return angle
-    else:
-        return 2 * np.pi - angle
-
-
-def calculateAngle(p1, p2, p3):
-    vec12 = p2-p1
-    vec23 = p2-p3
-    return angle(vec12, vec23)
-
-def calculateFinger(keypoint1, keypoint2, keypoint3, keypoint4, angleMin, angleMax):
-    a1 = calculateAngle(keypoint1, keypoint2, keypoint3)
-    a2 = calculateAngle(keypoint2, keypoint3, keypoint4)
-    fingerAngle = (a1+a2)/2.
-    #fingerAngle = a1
-    anglePercent = (fingerAngle - angleMin) / (angleMax - angleMin) * 100
-    return anglePercent
+    def __init__(self, baseKeyPoint):
+        self._baseKeyPoint = baseKeyPoint
     
-def processLandMarks(hand_landmarks, useKalman):
-    global lastTime, lastposition
-    keypoints = []
-    for data_point in hand_landmarks.landmark:
-        keypoints.append(np.array([data_point.x, data_point.y, data_point.z]))
+    def _angle(self, p1, p2):
+        ang1 = np.arctan2(*p1[::-1])
+        ang2 = np.arctan2(*p2[::-1])
+        return np.rad2deg((ang1 - ang2) % (2 * np.pi))
 
+    def calculateAngle(self, keyPoints):
+        base = keyPoints[self._baseKeyPoint]
+        direction = keyPoints[self._baseKeyPoint + 1]
+        zAxis = np.array([0., 0., 1.])
+        xAxis2d = np.array([1., 0.])
+        directionVec = direction - base
+        directionVec2d = np.array([directionVec[0], directionVec[2]])
+        handAngleDeg = self._angle(xAxis2d, directionVec2d)
+        handAnglePercent = handAngleDeg / 360. * 100.
+        return handAnglePercent
+
+
+class Finger(object):
+    def __init__(self, baseKeyPoint, angleMin, angleMax):
+        self._baseKeyPoint = baseKeyPoint
+        self._angleMin = angleMin
+        self._angleMax = angleMax
+
+    def _angle(self, v1, v2, acute = True):
+        angle = np.arccos(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
+        if (acute == True):
+            return angle
+        else:
+            return 2 * np.pi - angle
+
+    def _calculateAngleFrom3Points(self, p1, p2, p3):
+        vec12 = p2-p1
+        vec23 = p2-p3
+        return self._angle(vec12, vec23)
+
+    def calculateAngle(self, keyPoints):
+        a1 = self._calculateAngleFrom3Points(keyPoints[self._baseKeyPoint], keyPoints[self._baseKeyPoint + 1], keyPoints[self._baseKeyPoint + 2])
+        a2 = self._calculateAngleFrom3Points(keyPoints[self._baseKeyPoint+1], keyPoints[self._baseKeyPoint + 2], keyPoints[self._baseKeyPoint + 3])
+        fingerAngle = (a1+a2)/2.
+        #fingerAngle = a1
+        anglePercent = (fingerAngle - self._angleMin) / (self._angleMax - self._angleMin) * 100
+        return anglePercent
+
+def processLandMarks(keyPoints, fixedHandAngle):
     # map to 0-100
     angleMax = 3
     angleMin = 2
     
     position = {}
-    global lastKeyPoints
-    if lastKeyPoints != None:
-        varMeasure.addValue(lastKeyPoints[12][2])
-    lastKeyPoints = keypoints
-    print(varMeasure.getVariance())
-    if useKalman: 
-        for i in range (1, 6):
-            fingers[i].predict()
+    fingers = {}
+    fingers[1] = Finger(1, 2.3, 3.1) # use different percentage mapping for thumb
+    fingers[2] = Finger(5, angleMin, angleMax)
+    fingers[3] = Finger(9, angleMin, angleMax)
+    fingers[4] = Finger(13, angleMin, angleMax)
+    fingers[5] = Finger(17, angleMin, angleMax)
+    fingers[6] = HandAngle(0) # handAngle provides the same functions, but calculates the actual angle
 
-        fingers[1].update(keypoints[1], keypoints[2], keypoints[3], keypoints[4])
-        fingers[2].update(keypoints[5], keypoints[6], keypoints[7], keypoints[8])
-        fingers[3].update(keypoints[9], keypoints[10], keypoints[11], keypoints[12])
-        fingers[4].update(keypoints[13], keypoints[14], keypoints[15], keypoints[16])
-        fingers[5].update(keypoints[17], keypoints[18], keypoints[19], keypoints[20])
+    for i in range(1,7):
+        position[i] = fingers[i].calculateAngle(keyPoints)
 
-        position[1] = calculateFinger(fingers[1].getState(0),fingers[1].getState(1),fingers[1].getState(2),fingers[1].getState(3), 2.3, 3.) # use different percentage mapping for thumb
-        for i in range(2, 6):
-            position[i] = calculateFinger(fingers[i].getState(0),fingers[i].getState(1),fingers[i].getState(2),fingers[i].getState(3), angleMin, angleMax) # use different percentage mapping for thumb
-    else:
-        position[1] = calculateFinger(keypoints[1], keypoints[2], keypoints[3], keypoints[4], 2.3, 3.) # use different percentage mapping for thumb
-        position[2] = calculateFinger(keypoints[5], keypoints[6], keypoints[7], keypoints[8], angleMin, angleMax)
-        position[3] = calculateFinger(keypoints[9], keypoints[10], keypoints[11], keypoints[12], angleMin, angleMax)
-        position[4] = calculateFinger(keypoints[13], keypoints[14], keypoints[15], keypoints[16], angleMin, angleMax)
-        position[5] = calculateFinger(keypoints[17], keypoints[18], keypoints[19], keypoints[20], angleMin, angleMax)
-    velocity = {}
-    for i in range(1, 6):
+    for i in range(1, 7):
         uhand.setTargetPercent(i, position[i])
-    timenow = time.time()
+    
+    if fixedHandAngle:
+        uhand.setTargetPercent(6, 100)
 
     uhand.execute(0)
-
-    # store old data
-    lastTime = timenow
-    lastposition = position
     
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
 
+uhand = pyuhand.UHand("/dev/ttyUSB9")
+
+timedelta = 0.06
 # For webcam input:
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
         min_detection_confidence=0.7,
         min_tracking_confidence=0.5) as hands:
+    
     useKalman = True
+    fixedHandAngle = True
+    
+    filters = KeyPointFilter(21, timedelta)
     while cap.isOpened():
         success, image = cap.read()
         if not success:
@@ -197,14 +195,26 @@ with mp_hands.Hands(
             # only track first hand
             if(len(results.multi_hand_landmarks) > 0):
                 hand_landmarks = results.multi_hand_landmarks[0]
-                processLandMarks(hand_landmarks, useKalman)
+                keypoints = []
+                for data_point in hand_landmarks.landmark:
+                    keypoints.append(np.array([data_point.x, data_point.y, data_point.z]))
+                if useKalman: 
+                    filters.predict()
+                    filters.update(keypoints)
+                    keypoints = filters.getFilteredPoints()
+                processLandMarks(keypoints, fixedHandAngle)
                 mp_drawing.draw_landmarks(
                     image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
         cv2.imshow('MediaPipe Hands', image)
-        if cv2.waitKey(33) == ord('a'):
+        key = cv2.waitKey(int(timedelta * 1000. *0.9))
+        if key == ord('a'):
             useKalman = not useKalman
             print ("Use Kalman: %d" % (useKalman))
 
-        if cv2.waitKey(5) & 0xFF == 27:
+        if key == ord('b'):
+            fixedHandAngle = not fixedHandAngle
+            print ("Fix Hand Angle: %d" % (fixedHandAngle))
+
+        if key & 0xFF == 27:
             break
 cap.release()
