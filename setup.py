713c87bc5b20f0d5a04adc5157cdf6073beef960
Pete <github@kthxbye.us>
2021-04-28 23:15:18 +0800

    ADD: pip packaging

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
-from setuptools import setup
+#!/usr/bin/env python
 
-setup(name='pyuhand',
-      version='0.0.1',
-      description='Library to control uhand',
-      url='http://github.com/peteh/pyuhand',
-      author='Peter Hofmann',
-      author_email='github@kthxbye.us',
-      license='LGPL2.1',
-      packages=['pyuhand'],
-      zip_safe=False)
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
